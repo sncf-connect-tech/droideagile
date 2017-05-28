cd4a0d1d599b69c91336893533e5e72cac651f8d
Sylvain <sylvain.maillard6@gmail.com>
2017-05-28 23:34:52 +0200

    boot mouse management...

--- app/DroideAgile.py
+++ app/DroideAgile.py
@@ -1,8 +1,14 @@
+import pygame
+
+from app.droid_configuration import path_to_image
 from app.droide_ui import App, Screen
 
 
 class MainScreen(Screen):
-    pass
+    def render(self, display):
+        display.blit(pygame.image.load(path_to_image("background2.png")),(0,0))
+        pygame.draw.rect(display, (200,200,100),(0,0,50,100), 5)
+
 
 
 class DroideAgile(App):
--- app/droide_ui/__init__.py
+++ app/droide_ui/__init__.py
@@ -1,7 +1,7 @@
 import logging
-from pygame import mixer
 
 import pygame
+from pygame import mixer, MOUSEBUTTONDOWN
 from pygame.constants import QUIT
 
 from app.droid_configuration import init_configuration, droidConfig
@@ -10,12 +10,18 @@ from app.droid_configuration import init_configuration, droidConfig
 # a basic screen
 class Screen:
     def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+
+    def render(self, display):
         pass
+
+    def on_event(self, event, mouse_pos):
+        self.log.debug("got event " + str(event) + " at " + str(mouse_pos))
+        return self
 
 
 # a pygame app
 class App:
-
     def __init__(self, startup_screen, app_name="App"):
         self.current_screen = startup_screen
 
@@ -29,13 +35,20 @@ class App:
         pygame.init()
         self.clock = pygame.time.Clock()
 
-        self.width = droidConfig.getint("Screen","Width")
-        self.height = droidConfig.getint("Screen","Height")
+        self.real_width = droidConfig.getint("Screen", "RealWidth")
+        self.real_height = droidConfig.getint("Screen", "RealHeight")
         self.rotation = droidConfig.getint("Screen", "Rotation")
         self.use_full_screen = droidConfig.getboolean("Screen", "UseFullScreen")
         self.hide_mouse = droidConfig.getboolean("Screen", "HideMouse")
 
-        self.display = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN if self.use_full_screen else 0)
+        # actual display
+        self.display = pygame.display.set_mode((self.real_width, self.real_height),
+                                               pygame.FULLSCREEN if self.use_full_screen else 0)
+        # working display
+        # it is a surface like display but rotated
+        self.surface = pygame.transform.rotate(self.display, self.rotation)
+        self.logger.debug("center of new surface : " + str(self.surface.get_rect().center) + " rect is " + str(
+            self.surface.get_rect()))
         pygame.mouse.set_visible(not self.hide_mouse)
 
         # init sound system
@@ -43,27 +56,74 @@ class App:
 
         self.logger.info("App ready to display")
 
-
     # main loop here
     def run(self):
-        logging.debug("Running app")
+        self.logger.debug("Running app")
 
         # set up app here
         self.set_up()
 
         done = False
         while not done:
+
+            # clear screen
+            self.surface.fill((100, 255, 255))
+
+            # render current screen
+            self.current_screen.render(self.surface)
+
             for event in pygame.event.get():
                 if event.type == QUIT:
                     done = True
+                elif event.type == MOUSEBUTTONDOWN:
+                    # compute mouse position given rotation
+                    # bon ca marche pas bien mais bon... ca marche que avec 270....
+                    mouse_pos = pygame.mouse.get_pos()
+                    if self.rotation != 0:
+                        pos = rotate((0, 0), (mouse_pos[0],-mouse_pos[1]), math.radians(self.rotation))
+                        pos = (self.real_height  + pos[0], -pos[1])
+                    else:
+                        pos = mouse_pos
 
-        logging.debug("Stopping app")
+                    # pass event to current_screen
+                    new_screen = self.current_screen.on_event(event, pos)
+                    self.current_screen = new_screen
+
+            # tick to 60 fps
+            self.clock.tick(60)
+
+            # rotate surface
+            rotated = pygame.transform.rotate(self.surface, -self.rotation)
+
+            self.display.blit(rotated, (0, 0))
+
+            # flip to the screen
+            pygame.display.flip()
+
+        self.logger.debug("Stopping app")
         # clean up app here
         self.clean_up()
-        logging.debug("Exiting app")
+        self.logger.debug("Exiting app")
 
     def clean_up(self):
         pygame.quit()
 
     def set_up(self):
         pass
+
+
+import math
+
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
